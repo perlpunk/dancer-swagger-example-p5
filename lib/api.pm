1a5ed9c444dd855a153830b464ae0b174a39f25f
Tina Müller <cpan2@tinita.de>
2016-08-14 21:47:48 +0200

    demonstrate problem with numbers vs. strings
    and add comment about DBIx::Class::Helper::Row::NumifyGet

--- lib/api.pm
+++ lib/api.pm
@@ -19,7 +19,15 @@ my @menu = (
         category => 'main',
         name => 'Chili sin carne',
         description => '...',
-        price => 314,
+
+        # now if you're using MySQL or Sybase, for example, you would
+        # actually get numbers back as strings:
+        price => '314',
+        # The error message will be:
+        # "/0/price: Expected integer - got string."
+        # Fix: If you're using DBIx::Class, you're lucky:
+        # use DBIx::Class::Helper::Row::NumifyGet
+        # Otherwise you have to manually numify
     },
     {
         id => 2,
